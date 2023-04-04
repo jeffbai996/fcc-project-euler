1b39306aceaeaa3ad3b5d7bb236b83a9ac7d97bf
jeffbai996 <j.bai@wustl.edu>
2023-04-04 16:57:45 -0700

    Add complete code for Multiples of 3 and 5, delete .py files

--- multiples_3_5.js
+++ multiples_3_5.js
@@ -4,7 +4,13 @@
 // # Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
 
 function multiplesOf3and5(number) {
-
+  let totalSum = 0
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      totalSum += i;
+    }
+  }
+  return totalSum;
 }
 
 multiplesOf3and5(1000);
