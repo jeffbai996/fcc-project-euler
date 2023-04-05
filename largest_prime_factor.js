31bd3862850ac3b45c307ef063c08a8bc8fb7242
jeffbai996 <j.bai@wustl.edu>
2023-04-05 16:09:41 -0700

    Add exercise for Largest Prime Factor

---
+++ largest_prime_factor.js
@@ -0,0 +1,11 @@
+// Problem 3: Largest prime factor
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the given number?
+
+function largestPrimeFactor(number) {
+
+  return true;
+}
+
+largestPrimeFactor(13195);
