be2ec31e68573ba5be96f8716761f56a4fd1de82
jeffbai996 <j.bai@wustl.edu>
2023-04-10 12:10:58 -0700

    Remove solution for Largest Prime Factor

--- largest_prime_factor.js
+++ largest_prime_factor.js
@@ -5,23 +5,7 @@
 
 function largestPrimeFactor(number) {
   // define an array to store the prime factors
-  let primeFactors = [];
-  // start with 2 as the first prime factor
-  let i = 2;
-  // loop through the number
-  while (i <= number) {
-    // if the number is divisible by the current prime factor
-    if (number % i === 0) {
-      // push the prime factor to the array
-      primeFactors.push(i);
-      // divide the number by the prime factor
-      number /= i;
-    } else {
-      // increment if the number is not divisible by the current prime factor
-      i++;
-    }
-  }
-  return primeFactors[primeFactors.length - 1];
+
 }
 
 largestPrimeFactor(13195);
