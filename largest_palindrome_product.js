910637fd256717ecf9ad2e072b68744709420bb6
jeffbai996 <j.bai@wustl.edu>
2023-04-13 09:29:22 -0700

    Add comment for Largest Palindrome Product

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -4,18 +4,23 @@
 // Find the largest palindrome made from the product of two n-digit numbers.
 
 function isPalindrome(number) {
+  // convert number to string, split into array, reverse array, join array into string
   const strNumber = number.toString();
   const reversedStrNumber = strNumber.split('').reverse().join('');
   return strNumber === reversedStrNumber;
 }
 
 function largestPalindromeProduct(n) {
+  // find largest n-digit number
   const max = Math.pow(10, n) - 1;
   const min = Math.pow(10, n - 1);
+  // find largest palindrome product of two n-digit numbers
   let largestPalindrome = 0;
+  // loop through all possible combinations of n-digit numbers
   for (let i = max; i >= min; i--) {
     for (let j = max; j >= min; j--) {
       const product = i * j;
+      // if product is a palindrome and greater than largest palindrome, update largest palindrome
       if (isPalindrome(product) && product > largestPalindrome) {
         largestPalindrome = product;
       }
