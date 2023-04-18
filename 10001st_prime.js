144ab77ea5e5006c2543018bf04b6bbb3ca23ae5
jeffbai996 <j.bai@wustl.edu>
2023-04-18 09:33:33 -0700

    Add complete code for 1001st Prime

--- 10001st_prime.js
+++ 10001st_prime.js
@@ -3,9 +3,37 @@
 
 // What is the nth prime number?
 
-function nthPrime(n) {
-
+// function to check if a number is prime
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  if (num === 2) {
+    return true;
+  }
+  if (num % 2 === 0) {
+    return false;
+  }
+  const sqrt = Math.sqrt(num);
+  for (let i = 3; i <= sqrt; i += 2) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
   return true;
+}
+
+
+function nthPrime(n) {
+  let count = 0;
+  let i = 1;
+  while (count < n) {
+    i++;
+    if (isPrime(i)) {
+      count++;
+    }
+  }
+  return i;
 }
 
 nthPrime(10001);
