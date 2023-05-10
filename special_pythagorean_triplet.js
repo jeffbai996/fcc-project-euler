c273466f23998297527b5761bfd9696376002f8a
jeffbai996 <j.bai@wustl.edu>
2023-05-09 19:05:25 -0700

    First attempt at Special Pythagorean Triplet

--- special_pythagorean_triplet.js
+++ special_pythagorean_triplet.js
@@ -7,9 +7,40 @@
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc such that a + b + c = n.
 
 function specialPythagoreanTriplet(n) {
-  let sumOfabc = n;
- 
-  return true;
- }
+  // Declare variables
+  var a = 1;
+  var b = 2;
+  var c = 3;
+  var product = 0;
+  var sum = 0;
+  var found = false;
+  
+  while (!found) {
+    while (a < b) {
+      while (b < c) {
+        if (a + b + c === n) {
+          if (Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)) {
+            product = a * b * c;
+            found = true;
+            break;
+          }
+        }
+        c++;
+      }
+      if (found) {
+        break;
+      }
+      b++;
+      c = b + 1;
+    }
+    if (found) {
+      break;
+    }
+    a++;
+    b = a + 1;
+    c = b + 1;
+  }
+  return product;
+}
  
  specialPythagoreanTriplet(1000);
